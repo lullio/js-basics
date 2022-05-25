90563e73e8a3109e28f74be5c123040b7a42798f
Felipe PCINTEL W10 <felipelullio@gmail.com>
2022-05-25 17:20:56 -0300

    express - get request to home page - html content

--- express.js
+++ express.js
@@ -3,4 +3,24 @@ let express = require("express"); // vai olhar na pasta node_modules e procurar 
 // cria um express server websever
 let ourApp = express();
 
+// o que fazer se tiver um GET REQUEST para home page
+ourApp.get('/', function (req, res){// 1º url 2º funcao q vai rodar sempre pra essa url
+   res.send(`
+      <form action="/answer" method="POST"> <!-- redireciona para /answer-->
+         <p>What Color is the sky on a clear and sunny day?</p>
+         <input name="skyColor" autocomplete="off" type="text">
+         <button>Submit Answer</button>
+      </form>
+   `);
+}) 
+   
+
 ourApp.listen(3000) // listen incoming request
+
+/* 
+GET = enviar uma requisição get
+qdo acessa google.com seu browser enviou uma requisição get para os servidores google, ao clicar urls vc faz um get request
+GET INFORMATION
+
+POST = Postar informação, post data
+*/
